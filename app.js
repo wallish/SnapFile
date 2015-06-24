9e7ec1f178d275a9b3ef414cb92fcccbee9bf34c
fanorazafi <fanorazafindramaro@MacBook-Pro-de-Fano.local>
2015-06-24 15:14:15 +0200

    ajout de la gestion de l'upload

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 var express = require('express');
 var app = express();
+var multer  = require('multer');
+var done=false;
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
+
+app.use(multer({ dest: './uploads/',
+	rename: function (fieldname, filename) {
+		return filename+Date.now();
+	},
+	onFileUploadStart: function (file) {
+		console.log(file.originalname + ' is starting ...')
+	},
+	onFileUploadComplete: function (file) {
+		console.log(file.fieldname + ' uploaded to  ' + file.path)
+		done=true;
+	}
+}));
+
+
 
 // index page 
 app.get('/', function(req, res) {
     res.render('pages/index');
+});
+
+app.post('file/upload',function(req,res){
+	if(done==true){
+		console.log(req.files);
+		res.end("File uploaded.");
+	}
 });
 
 
